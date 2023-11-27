3083996ed2c68143b9b277dd1252139077a76c34
vijithahmbandara <vijithahmbandara@gmail.com>
2023-11-27 19:38:28 +0530

    start

---
+++ utils/validator.js
@@ -0,0 +1,31 @@
+const validator = (reservationData) => {
+  const warnings = [];
+
+  if (!/^\d{4}-\d{2}-\d{2}$/.test(reservationData["date"])) {
+    warnings.push("Date is invalid");
+  }
+
+ /*  if (/^(?:[01]\d|2[0-3]):[0-5]\d$/.test(reservationData["time"])) {
+    warnings.push("Time is invalid");
+  } */
+
+  if (!reservationData["location"]) {
+    warnings.push("Location is invalid");
+  }
+
+  if (!/^[A-Z0-9\s-]*$/.test(reservationData["vehicle_no"])) {
+    warnings.push("Vehicle number is invalid");
+  }
+
+  if (!/^\d+$/.test(reservationData["mileage"])) {
+    warnings.push("Mileage is invalid");
+  }
+
+  if (!reservationData["message"]) {
+    warnings.push("Message is invalid");
+  }
+
+  return warnings;
+};
+
+module.exports = validator;
